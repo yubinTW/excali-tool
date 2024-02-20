24d8c33e8a804aac1309be6e2cc3161faeb578a0
yubintw <yubin.cs05g@nctu.edu.tw>
2024-02-21 00:21:00 +0800

    test: to assert id field

--- test/tool.test.ts
+++ test/tool.test.ts
@@ -21,42 +21,7 @@ describe('Tool Testing', () => {
       ]
       const convertedElements = convertToExcalidrawElements(rawData)
       expect(convertedElements).toHaveLength(1)
-      expect(convertedElements[0]).toMatchObject({
-        type: 'text',
-        x: 100,
-        y: 100,
-        width: 98.93000030517578,
-        height: 25,
-        angle: 0,
-        strokeColor: '#1e1e1e',
-        backgroundColor: 'transparent',
-        fillStyle: 'solid',
-        strokeWidth: 2,
-        strokeStyle: 'solid',
-        roughness: 1,
-        opacity: 100,
-        groupIds: [],
-        frameId: null,
-        roundness: null,
-        version: 1,
-        versionNonce: 0,
-        isDeleted: false,
-        boundElements: null,
-        link: null,
-        locked: false,
-        text: 'Hello world',
-        fontSize: 20,
-        fontFamily: 1,
-        textAlign: 'left',
-        verticalAlign: 'top',
-        baseline: 18,
-        containerId: null,
-        originalText: 'Hello world',
-        lineHeight: 1.25,
-        customData: {
-          createdBy: 'user1'
-        }
-      })
+      expect(convertedElements[0]).toHaveProperty('id')
     })
   })
 
