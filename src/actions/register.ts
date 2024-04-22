274c6bfeb0aeaa2ecd3c36454b4c056b1b4e4503
yubintw <yubin.cs05g@nctu.edu.tw>
2024-04-22 23:40:36 +0800

    style: fix prettier

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,10 +1,10 @@
-import { Action } from "./types";
+import { Action } from './types'
 
-export let actions: readonly Action[] = [];
+export let actions: readonly Action[] = []
 
 export const register = <T extends Action>(action: T) => {
-  actions = actions.concat(action);
+  actions = actions.concat(action)
   return action as T & {
-    keyTest?: unknown extends T["keyTest"] ? never : T["keyTest"];
-  };
-};
+    keyTest?: unknown extends T['keyTest'] ? never : T['keyTest']
+  }
+}
